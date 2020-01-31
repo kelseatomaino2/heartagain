a3a79b71e5a387c90c0968cbfd932b7f9e792cb2
Kelsea Tomaino <kelseatomaino@v1040-wn-rt-c-112-129.campus-dynamic.uwaterloo.ca>
2020-01-31 08:57:29 -0500

    Finishing Search bar

--- sensor/views.py
+++ sensor/views.py
@@ -14,7 +14,7 @@ from sensorWorker.models import Session
 def home(request):
 
     return render(request, 'home.html', {
-        'date': datetime.datetime.now(),
+        'date': datetime.now(),
     })
 
 def sensor(request):
@@ -38,15 +38,13 @@ class ChartData(APIView):
 
     def get(self, request, format=None):
         user_id = 'testid'
-        date = datetime.date(year=2019, month=11, day=11)
+        date = datetime(year=2019, month=11, day=11)
         data = HistoricalEcgData.retrieve_data(user_id, date)
         return Response(data)
 
 class search(TemplateView):
 
     def get(self, request):
-        values = EcgData.objects.all().filter(user_id='testid')
-        print(values)
         return render(request, 'search.html',)
 
     def post(self, request):
@@ -60,21 +58,22 @@ class search(TemplateView):
         validation_check = self.check_if_valid(post_data)
         args = {}
         args['is_valid'] = validation_check['is_valid']
+        args['message'] = validation_check['message']
         if args['is_valid']==False:
             return render(request, 'search.html', args) 
-        else:   
+        else:
             args['results'] = self.query_database(post_data)
             return render(request, 'search.html', args)
 
     def check_if_valid(self, post_data):
         validation_check = {}
         if(post_data['user_id']):
-            values = Session.objects.all().filter(user_id=user_id)
+            values = Session.objects.all().filter(user_id=post_data['user_id'])
             if not values:
                 validation_check['is_valid'] = False
-                validation_check['message'] = "That user_id does not exist, please try again"
+                validation_check['message'] = "That ID does not exist, please try again"
                 return validation_check
-        elif (post_data['start_date'])
+        if (post_data['start_date']):
             try:
                 if (post_data['start_time']):
                     start_date_time_str = post_data['start_date'] + " " + post_data['start_time']
@@ -87,9 +86,9 @@ class search(TemplateView):
                 validation_check['message'] = "Improper data, please check date and time formats"
                 return validation_check
        
-        elif (post_data['end_date'])
+        if (post_data['end_date']):
             try:
-                if(post_data['end_time'])
+                if(post_data['end_time']):
                     end_date_time_str = post_data['end_date'] + " " + post_data['end_time']
                     end_date_time = datetime.strptime(end_date_time_str, "%Y-%m-%d %H:%M:%S")
                 else:
@@ -104,59 +103,61 @@ class search(TemplateView):
         validation_check['message'] = "Success"
         return validation_check
 
-    def get_datetime(self, start_or_end, post_data):
-       if post[start_or_end]:
+    def get_datetime_str(self, start_or_end, post_data):
+        if post_data[start_or_end + "_date"] != '':
             date_str = post_data[start_or_end+"_date"] + " " + post_data[start_or_end + "_time"]
             date_time = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
         else:
+            print(post_data)
             date_str = post_data[start_or_end+"_date"] + " 0:0:0"
             date_time = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
         return date_time
 
     def query_database(self, post_data):
-        if(post_data['user_id'] != None and post_date['start_date']==None and post_data['end_date']==None):
-            values = Session.objects.all().filter(user_id=user_id)
+        if(post_data['user_id'] != '' and post_data['start_date']=='' and post_data['end_date']==''):
+            values = Session.objects.all().filter(user_id=post_data['user_id'])
 
-        elif(post_data['user_id'] != None and post_data['start_date'] != None and post_data['end_date']==None):
+        elif(post_data['user_id'] != '' and post_data['start_date'] != '' and post_data['end_date']==''):
             start_date_time = self.get_datetime_str("start", post_data)
-            values = Session.objects.all().filter(user_id=user_id, start_date=start_date_time)
+            values = Session.objects.all().filter(user_id=post_data['user_id'], start_date=start_date_time)
 
-        elif(post_data['user_id'] != None and post_data['start_date'] == None and post_data['end_date']!=None):
+        elif(post_data['user_id'] != '' and post_data['start_date'] == '' and post_data['end_date']!=''):
             end_date_time = self.get_datetime("end", post_data)
-            values = Session.objects.all().filter(user_id=user_id, end_date=end_date_time)
+            values = Session.objects.all().filter(user_id=post_data['user_id'], end_date=end_date_time)
 
-        elif(post_data['user_id'] != None and post_data['start_date'] != None and post_data['end_date'] != None):
+        elif(post_data['user_id'] != '' and post_data['start_date'] != '' and post_data['end_date'] != ''):
             start_date_time = self.get_datetime_str("start", post_data)
             end_date_time = self.get_datetime_str("end", post_data)
-            values = Session.objects.all().filter(user_id=user_id, start_date=start_date_time, end_date=end_date_time)
+            values = Session.objects.all().filter(user_id=post_data['user_id'], start_date=start_date_time, end_date=end_date_time)
 
-        elif(post_data['user_id'] == None and post_data['start_date'] != None and post_data['end_date']==None):
+        elif(post_data['user_id'] == '' and post_data['start_date'] != '' and post_data['end_date']==''):
             start_date_time = self.get_datetime_str("start", post_data)
             values = Session.objects.all().filter(start_date=start_date_time)
 
-         elif(post_data['user_id'] == None and post_data['start_date'] == None and post_data['end_date']!=None):
+        elif(post_data['user_id'] == '' and post_data['start_date'] == '' and post_data['end_date']!=''):
             end_date_time = self.get_datetime("end", post_data)
             values = Session.objects.all().filter(end_date=end_date_time)
 
-        elif(post_data['user_id'] == None and post_data['start_date'] != None and post_data['end_date'] != None):
+        elif(post_data['user_id'] == '' and post_data['start_date'] != '' and post_data['end_date'] != ''):
             start_date_time = self.get_datetime_str("start", post_data)
             end_date_time = self.get_datetime_str("end", post_data)
             values = Session.objects.all().filter(start_date=start_date_time, end_date=end_date_time)
         else:
             values = Session.objects.all()
 
-        data = self.make_data_dictionary(values)
+        data = self.make_data_dictionary_list(values)
+        print(data)
         return data
     
-    def make_data_dictionary(self, values):
+    def make_data_dictionary_list(self, values):
         data = []
         for data_entry in values:
             row_dict = {}
             row_dict['user_id'] = data_entry.user_id
             row_dict['start_date'] = datetime.strftime(data_entry.start_date, "%Y-%m-%d")
-            row_dict['start_time'] = datetime.strftime(data_entry.start_time, "%H:%M:%S")
+            row_dict['start_time'] = datetime.strftime(data_entry.start_date, "%H:%M:%S")
             row_dict['end_date'] = datetime.strftime(data_entry.end_date, "%Y-%m-%d")
-            row_dict['end_time'] = datetime.strftime(data_entry.end_time, "%H:%M:%S")
+            row_dict['end_time'] = datetime.strftime(data_entry.end_date, "%H:%M:%S")
             data.append(row_dict)
 
         return data
