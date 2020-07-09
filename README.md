4b30e3de64a622c15eef2953b17b2fc6598dfd1c
Kelsea Tomaino <kelseatomaino@Kelseas-MBP.cogeco.local>
2020-07-09 11:33:40 -0400

    Updating readme and writes to database in sensorWorker

--- README.md
+++ README.md
@@ -1,12 +1,68 @@
-# heartagain
+# heartagain 
 
-### Starting the App - By Kelsea
+### Overview
+The purpose of this application is to monitor the state of an ex vivo heart during transportation. It stores incoming data from sensor readings to be queried at a later date. 
 
-SSH into pi `ssh pi@172.20.10.4`
-Password: `heartagain`
+### Features
+The web application uses a background task and the GPiO library to read from ECG, temperature, flow and pressure sensors. The data is displayed using web sockets. The data is stored in a relational database and there is a search tool to filter historical data.
+
+### Running the App Locally
+
+1. Git clone the repository
+2. Cd into the project directory 
+3. Create a virtual environment
+`virtualenv heartenv`
+4. Activate the environment
+`source heartenv bin activate`
+5. Install the requirements 
+`pip install -r requirments.txt`
+6. Install postgres and create a database with tables matching the models in sensorWorker/models.py 
+7. Run migrations 
+`python manage.py migrate`
+
+#### To run the app without the Raspberry Pi
+8. Comment out any lines of code using the GPiO library
+9. Cd into the project directory
+10. Acitvate the virtual environment
+11. Start the redis server 
+`redis-server`
+12. Start the background task
+`python3 manage.py readSensor`
+13. Start the web app
+`python3 manage.py runserver`
+14. Navigate to the URL and add/home to the end
+
+#### To run the app with the Raspberry Pi 
+15. SSH into pi `ssh pi@172.20.10.4` (Password: `heartagain`)
+16. Start the background task 
+`python3 manage.py readSensor`
+17. Start the web app
+`python3 manage.py runserver 172.20.10.4`
+18. Navigate to link 172.20.10.4:8000/home/
+
 
 To start background task, `tmux` and to disconnect `Ctrl + b` then to reattach `tmux a` to create a new window `Ctrl %` then to switch between windows `Ctrl ARROW_OF_CHOICE`
-Then run `python3 manage.py readSensor`
-Start webapp `python3 manage.py runserver 172.20.10.4`
 
-Navigate to link 172.20.10.4:8000/home/
+#### Loading fake data into database for testing
+1. Load fake AEG data
+`python3 loadAEGData.py`
+2. Load fake ECG data 
+`python3 loadFakeData.py`
+
+### Dependencies
+* PostgreSQL
+* Redis
+* Django
+* Django Channels
+* Chart.js
+
+### ToDo List
+* Update BPM calculation in readSensor.py
+* Integrate the use of all sensors and reading from them
+* Update logic for the search tool 
+* Display all historical data based on search criteria-> ECG, temperature, flow and pressure
+* Create Docker container for the application to run in to avoid missing dependencies and wrong versioning
+* Enable sign in features
+
+
+
--- sensorWorker/management/commands/readSensor.py
+++ sensorWorker/management/commands/readSensor.py
@@ -1,12 +1,14 @@
 from django.core.management import BaseCommand
 from gpiozero import MCP3008
 import time, sys
-#import RPi.GPIO as GPIO
-# import numpy as np
+import RPi.GPIO as GPIO
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 from channels.generic.websocket import WebsocketConsumer
 from sensorWorker.models import EcgData
+from sensorWorker.models import FlowData
+from sensorWorker.models import Session
+import datetime
 
 
 # This class must be named Command, and subclass BaseCommand
@@ -18,47 +20,55 @@ class Command(BaseCommand):
     # A command must define handle()
     def handle(self, *args, **options):
         self.channel_layer = get_channel_layer()
+
+        # This method is a background task used to read sensor outputs, send values to the UI for display
+        # Values are stored in the database 
         
-        #Setup pins - AD
-        #ECG_output = MCP3008(channel=0, device=0)
-        #flow_out = MCP3008(channel=1, device=0) #when using the transonic, test sensor is digital
-        #flow_in = MCP3008(channel=2, device=0) #when using the transonic, test sensor is digital
-        #pressure_oxy = MCP3008(channel=3, device=0)
-        #pressure_deoxy = MCP3008(channel=4, device=0)
-        #pressure_negative = MCP3008(channel=5, device=0)
-        #temperature = MCP3008(channel=6, device=0)
+        #Setup pins - AD 
+        # These pins are used with the ADC for analog sensors
+        #self.ECG_output = MCP3008(channel=0, device=0)
+        #self.flow_out = MCP3008(channel=1, device=0) #when using the transonic, test sensor is digital
+        #self.flow_in = MCP3008(channel=2, device=0) #when using the transonic, test sensor is digital
+        #self.pressure_oxy = MCP3008(channel=3, device=0)
+        #self.pressure_deoxy = MCP3008(channel=4, device=0)
+        #self.pressure_negative = MCP3008(channel=5, device=0)
+        #self.temperature = MCP3008(channel=6, device=0)
         
         #Setup up pins - digital
-        #flow_sensor = 23
-        #GPIO.setmode(GPIO.BCM)
-        #GPIO.setup(flow_sensor, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        #GPIO.setup(22,GPIO.IN) # LOD+
-        #GPIO.setup(27,GPIO.IN) # LOD-
+        flow_sensor = 23
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(flow_sensor, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        GPIO.setup(22,GPIO.IN) # LOD+
+        GPIO.setup(27,GPIO.IN) # LOD-
         
         #Set up ECG variables        
         self.BPM = 0
         self.ECG = 0
         self.flow = 0
         self.beat_old = 0
-        self.beats = [0] * 4 #np.zeros(500)  # Used to calculate average BPM, creates a 1D array of size 500
+        self.beats = [0] * 4 
         self.beatIndex = 0
         self.threshold = 0.9  #Threshold at which BPM calculation occurs
         self.belowThreshold = True
         
         self.count = 0
+        # This is used to simulate ECG data. These are fake values in database
         self.aeg_values = EcgData.objects.all().filter(user_id='testid2')
         self.total_aeg_values = self.aeg_values.count()
+        self.user_id = Session.objects.latest('start_date').user_id
+
 
         def count_pulse(channel):
             self.count = self.count + 1
             return self.count
         
-        #GPIO.add_event_detect(flow_sensor, GPIO.FALLING, callback=count_pulse)
+        GPIO.add_event_detect(flow_sensor, GPIO.FALLING, callback=count_pulse)
 
         self.room_group_name = 'sensor'
         x = 0
         i = 0
         flow_alert = False
+        # This is an infinite loop that is 
         while True: 
             if(i==self.total_aeg_values):
                 i = 0
@@ -82,8 +92,9 @@ class Command(BaseCommand):
                 x = 0
             else:
                 x = x+1
-            #pressure =  pressure_oxy.value
-            #temperature = temperature.value
+            pressure =  pressure_oxy.value
+            temperature = temperature.value
+
             if(self.flow==0):
                 flow_alert = True
             else:
@@ -94,8 +105,10 @@ class Command(BaseCommand):
             self.stdout.write("ECG reading..." + str(self.ECG))
             self.stdout.write("BPM reading..." + str(self.BPM))
             self.stdout.write("Flow reading..." + str(self.flow))
-            #self.stdout.write("Flow reading..." + str(pressure))
-            #self.stdout.write("Flow reading..." + str(temperature))
+
+            # These needed to be added when these sensors are connected
+            #self.stdout.write("Pressure reading..." + str(pressure))
+            #self.stdout.write("Temperature reading..." + str(temperature))
 
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
@@ -108,6 +121,7 @@ class Command(BaseCommand):
                     #'temperature_value': temperature,
                 }
             )
+            # This can be changed based on how frequently sensors need to be read 
             time.sleep(0.5)
                   
     def take_ecg_reading(self):
@@ -117,12 +131,28 @@ class Command(BaseCommand):
             self.ECG = ECG_output.value
             print("ECG Connected, ECG: ", self.ECG)
 
+    def insert_into_database(self, id, ecg_value=None, flow_value=None, pressure_value=None, temperature_value=None):
+        if(ecg_vale):
+            ecg = EcgData(user_id=self.session_id, date_time=datetime.datetime.now() 
+                ecg_value=ecg_value)
+            ecg.save()
+        else if(flow_value):
+            flow = FlowData(user_id=self.session_id, date_time=datetime.datetime.now(), 
+                flow_value=flow_value)
+            flow.save()
+        else:
+            ("Print no value to insert")
+        # TODO: Make if statements for remaining sensor values -> import models to insert into db tables
+
+
     def calculate_BPM(self):
         # get current time
         beat_new = int(round(time.time() * 1000))
         diff = beat_new - self.beat_old
-        currentBPM = 60000 / diff;    #convert to BPM
-        self.beats[self.beatIndex] = currentBPM #store for avg
+        # convert to bpm
+        currentBPM = 60000 / diff;   
+        # store for average calculation
+        self.beats[self.beatIndex] = currentBPM 
         total = sum(self.beats)
         BPM = total / 4
         self.beat_old = beat_new;
